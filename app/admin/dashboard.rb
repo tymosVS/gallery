112e189ede18c9f62b11a15630b6a1bc25d66374
Vladimir <timos9vs@gmail.com>
2019-07-29 15:16:32 +0300

    Remove create common category

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -2,31 +2,63 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
 
   content title: proc { I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
-      end
-    end
+  #   div class: "blank_slate_container", id: "dashboard_default_message" do
+  #     span class: "blank_slate" do
+  #       span I18n.t("active_admin.dashboard_welcome.welcome")
+  #       small I18n.t("active_admin.dashboard_welcome.call_to_action")
+  #     end
+  #   end
 
     # Here is an example of a simple dashboard with columns and panels.
     #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
 
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
+    columns do
+      column do
+        panel "Last categories" do
+          ul do
+            Category.last(5).map do |post|
+              li link_to(post.title, categories_path(post))
+            end
+          end
+        end
+      end
+
+      columns do
+        column do
+          panel "Last comments" do
+            div do
+              Comment.last(5).map do |post|
+              div do
+                p link_to(post.title, comments_path(post))
+                p post.body
+              end
+            end
+              
+                
+              end
+            
+          end
+        end
+      end
+
+      columns do
+        column do
+          panel "Last images" do
+            
+              Image.last(10).map do |image|
+                li image_tag(image.image.medium_thumb.url, alt: 'Image')
+                li image.title
+              end
+            
+          end
+        end
+      end
+
+      column do
+        panel "User actions" do
+          para "Actions informations"
+        end
+      end
+    end
   end # content
 end
