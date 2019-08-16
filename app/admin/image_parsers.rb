764f76bbb51fcc7e15279dde8654cb5bb4ccc2df
Vladimir <timos9vs@gmail.com>
2019-08-16 11:18:55 +0300

    remove image scraper page

--- app/admin/image_parsers.rb
+++ app/admin/image_parsers.rb
@@ -2,13 +2,16 @@ ActiveAdmin.register ImageParser do
   permit_params :site_path, :id, :image_url
 
   batch_action :import do |selection|
+    # Dir.chdir("#{Rails.root}/public/loads")
+    FileUtils.mkdir_p('site')
+    
     ImageParser.find(selection).each do |image_parser|
+
       image_parser.destroy
     end
+    FileUtils.rm_r('site')
     redirect_to admin_image_parsers_path 
   end
-
-
 
   form title: 'Create ImageParser' do |f|
     f.inputs 'ImageLoaders' do
@@ -16,41 +19,40 @@ ActiveAdmin.register ImageParser do
     end
     actions
   end
-  
 
   controller do
 
-
-      def create
-        # ImageParser.new(site_path: params["image_parser"]["site_path"]).save
-        flash[:notice] = 'Team created successfully'
-        url = params["image_parser"]["site_path"]
-        base = URI.parse(url.to_s)
-        html = open(url)
-        doc = Nokogiri::HTML(html)
-        Dir.chdir("#{Rails.root}/public/loads")
-        FileUtils.mkdir_p(base.host)
-        doc.search('img').each do |img|
-          src = img[:src]
-          normalized = base.merge(URI.parse(src)).to_s
-          # raise dddd
-          u = Image.new
-          u.image = normalized
+    def create
+      flash[:notice] = 'Team created successfully'
+      url = params["image_parser"]["site_path"]
+      base = URI.parse(url.to_s)
+      html = open(url)
+      doc = Nokogiri::HTML(html)
+      Dir.chdir("#{Rails.root}/public/loads")
+      FileUtils.mkdir_p(base.host)
+      doc.search('img').each do |img|
+        src = img[:src]
+        normalized = base.merge(URI.parse(src)).to_s
+        # raise dddd
+        u = Image.new
+        u.image = normalized
+        if normalized =~ /(\.((jpg)|(png)|(jpeg)))$/
           ImageParser.new(site_path: url, image_url: normalized).save
-          # filename = Pathname.new(normalized).basename.to_s
-          # open(normalized) do |img_file|
-          #   File::open(base.host + '/' + filename, 'wb') do |f|
-          #     f.write(img_file.read)
-          #   end
-          # end
         end
-        # redirect_back(fallback_location: root_path)
-        redirect_to admin_image_parsers_path
-        # redirect_to request.referrer
-      # end
+        # filename = Pathname.new(normalized).basename.to_s
+        # open(normalized) do |img_file|
+        #   File::open(base.host + '/' + filename, 'wb') do |f|
+        #     f.write(img_file.read)
+        #   end
+        # end
       end
-    
+      # redirect_back(fallback_location: root_path)
+      redirect_to admin_image_parsers_path
+      # redirect_to request.referrer
+    # end
     end
+  end
+
     index do
       selectable_column 
       column :id
