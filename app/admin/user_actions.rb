932b923e5df3c818c3386877cacbdd2c3a717513
Vladimir <timos9vs@gmail.com>
2019-09-02 15:40:25 +0300

    rm user_act

--- app/admin/user_actions.rb
+++ app/admin/user_actions.rb
@@ -1,38 +1,38 @@
-ActiveAdmin.register UserAction,:as => "UserActions" do
+# ActiveAdmin.register UserAction,:as => "UserActions" do
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:permitted, :attributes]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  # index do
-  #   column :id
-  #   column :user_id
-  #   # column User_email
-  #   column :email
-  #   column :action
-  #   column :action_path
-  # end
-  filter :action
-  filter :user,
-    as: :select, 
-    label: 'User email',
-    collection: User.all.pluck(:email)
+#   # See permitted parameters documentation:
+#   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#   #
+#   # permit_params :list, :of, :attributes, :on, :model
+#   #
+#   # or
+#   #
+#   # permit_params do
+#   #   permitted = [:permitted, :attributes]
+#   #   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   #   permitted
+#   # end
+#   # index do
+#   #   column :id
+#   #   column :user_id
+#   #   # column User_email
+#   #   column :email
+#   #   column :action
+#   #   column :action_path
+#   # end
+#   filter :action
+#   filter :user,
+#     as: :select, 
+#     label: 'User email',
+#     collection: User.all.pluck(:email)
 
-  index do
-    selectable_column
-    column :id
-    column :user
-    column :action
-    column :action_path
-    column :created_at
-    actions
-  end
-end
+#   index do
+#     selectable_column
+#     column :id
+#     column :user
+#     column :action
+#     column :action_path
+#     column :created_at
+#     actions
+#   end
+# end
