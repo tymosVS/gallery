d44a93a6e84440ac13e144f81b52ca1fe453ae27
Vladimir <timos9vs@gmail.com>
2019-09-03 09:33:22 +0300

    add mail config to prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,17 @@ Rails.application.configure do
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
-  config.i18n.fallbacks = true
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'mail.google.com',
+  user_name:            ENV['MAILER_EMAIL'],
+  password:             ENV['MAILER_PASSWORD'],
+  authentication:       :plain,
+  enable_starttls_auto: true
+}
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
