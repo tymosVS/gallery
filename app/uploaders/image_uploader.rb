c1c90c8e8746784a4edb709210a4ed455831615f
Vladimir <timos9vs@gmail.com>
2019-09-03 17:03:53 +0300

    removed images

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -6,7 +6,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   storage :file
 
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "public/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   version :small_thumb do
