a26c7d33e217d35552c9c2d02217ac39e35c86b0
Vladimir <timos9vs@gmail.com>
2019-09-06 10:18:05 +0300

    change redis conf

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,5 +1,5 @@
 # uri = URI.parse("redis://localhost:6379/")  
 # Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 # Dir["#{Rails.root}/app/jobs/*.rb"].each { |file| require file }
-uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:6379/" )
+uri = ENV["REDISTOGO_URL"] || "redis://localhost:6379/"
 REDIS = Redis.new(:url => uri)
