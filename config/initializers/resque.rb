2609a69845fca9afabd8fce880b27268141fe7e2
Vladimir <timos9vs@gmail.com>
2019-09-06 10:53:23 +0300

    try red

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,3 +1,3 @@
-uri = URI.parse("redis://localhost:6379/")  
+uri = URI.parse(ENV['REDIS_URL'] ||"redis://localhost:6379/")  
 Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 Dir["#{Rails.root}/app/jobs/*.rb"].each { |file| require file }
