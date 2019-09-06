847be4fb1ad4c6faadc65e3e95c5dd4b2e892638
Vladimir <timos9vs@gmail.com>
2019-09-06 10:48:15 +0300

    try redis

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,5 +1,3 @@
-# uri = URI.parse("redis://localhost:6379/")  
-# Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-# Dir["#{Rails.root}/app/jobs/*.rb"].each { |file| require file }
-uri = ENV["REDISTOGO_URL"] || "redis://localhost:6379/"
-REDIS = Redis.new(:url => uri)
+uri = URI.parse("redis://localhost:6379/")  
+Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+Dir["#{Rails.root}/app/jobs/*.rb"].each { |file| require file }
