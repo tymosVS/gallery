380a5314ddac23c32533fd03543c24c213a08c8d
Vladimir <timos9vs@gmail.com>
2019-09-06 13:09:02 +0300

    proc

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,3 +1,7 @@
-uri = URI.parse(ENV['REDIS_URL'] ||"redis://localhost:6379/")  
+if Rails.env.production?
+  uri = URI.parse(ENV['REDIS_URL'])  
+else
+  uri = URI.parse("redis://localhost:6379/")  
+end
 Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 Dir["#{Rails.root}/app/jobs/*.rb"].each { |file| require file }
