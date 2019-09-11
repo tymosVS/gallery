b8d77c6afe379ed5630cca0b225ec1bfbe9bfda9
Vladimir <timos9vs@gmail.com>
2019-09-11 17:34:03 +0300

    change style in home_page

--- app/controllers/home_pages_controller.rb
+++ app/controllers/home_pages_controller.rb
@@ -2,7 +2,9 @@ class HomePagesController < ApplicationController
   def index
     @images = {}
     @top_categories.each do |top_cat|
-      @images[top_cat] = top_cat.images.first
+      @images[top_cat] = top_cat.images.first(3)
     end
+    @statistic = [Image.count, Fan.count, Comment.count]
+
   end
 end
