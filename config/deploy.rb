9b3b763db002c8b6c1d4c19b8a7d89698a82e138
Vladimir <timos9vs@gmail.com>
2019-09-17 17:05:40 +0300

    changes socket path

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,7 +15,7 @@ set :rvm_ruby_version, 'ruby-2.6.3@gallery-gemset'
 set :puma_rackup, -> { File.join(current_path, 'config.ru') }
 set :puma_state, "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
-set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"
+set :puma_bind, "#{shared_path}/tmp/sockets/puma.sock"
 set :puma_conf, "#{shared_path}/puma.rb"
 set :puma_access_log, "#{shared_path}/log/puma_error.log"
 set :puma_error_log, "#{shared_path}/log/puma_access.log"
