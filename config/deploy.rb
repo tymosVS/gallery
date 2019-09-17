5607545f57e0944ff23723753b79b82958dd2b53
Vladimir <timos9vs@gmail.com>
2019-09-17 14:56:38 +0300

    try #32

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.11.1"
 
 set :application, 'gallery'
-set :repo_url, 'git@github.com:tymosVS/gallery.git' # Edit this to match your repository
+set :repo_url, 'git@github.com:tymosVS/gallery.git'
 set :branch, :master
 set :deploy_to, '/home/deploy/gallery'
 set :pty, true
@@ -10,16 +10,17 @@ set :linked_files, %w{config/database.yml config/application.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 set :keep_releases, 5
 set :rvm_type, :user
-set :rvm_ruby_version, 'ruby-2.6.3@gallery-gemset' # Edit this if you are using MRI Ruby
+set :rvm_ruby_version, 'ruby-2.6.3@gallery-gemset' 
 
 set :puma_rackup, -> { File.join(current_path, 'config.ru') }
 set :puma_state, "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
-set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"    #accept array for multi-bind
+set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"
 set :puma_conf, "#{shared_path}/puma.rb"
 set :puma_access_log, "#{shared_path}/log/puma_error.log"
 set :puma_error_log, "#{shared_path}/log/puma_access.log"
 set :puma_role, :app
+set :puma_port, 80
 set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
 set :puma_threads, [0, 8]
 set :puma_workers, 0
