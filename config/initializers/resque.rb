e0e604d7c4f741d5e68a618094646879ba5a1ec1
Vladimir <timos9vs@gmail.com>
2019-09-20 12:58:41 +0300

    try add redis to aws

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,7 +1,7 @@
-# if Rails.env.production?
-#   uri = URI.parse(ENV['REDIS_URL'])  
-# else
-#   uri = URI.parse("redis://localhost:6379/")  
-# end
-# Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-# Dir["#{Rails.root}/app/jobs/*.rb"].each { |file| require file }
+if Rails.env.production?
+  uri = URI.parse(ENV['REDIS_URL'])  
+else
+  uri = URI.parse("redis://localhost:6379/")  
+end
+Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+Dir["#{Rails.root}/app/jobs/*.rb"].each { |file| require file }
